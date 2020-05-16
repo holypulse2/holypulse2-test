2d6aa4827e39210b58eca515ed387ce895da628c
Umer <holypulse2@gmail.com>
2020-05-16 12:34:27 +0100

    Updates

---
+++ precache-manifest.e14ccbcf1fcf6577baf267dda7169f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331b2f69c5e06b053562ac17cde8a384",
+    "url": "/holypulse2-test/index.html"
+  },
+  {
+    "revision": "0772f7abb03a49845fa3",
+    "url": "/holypulse2-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/holypulse2-test/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/holypulse2-test/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0772f7abb03a49845fa3",
+    "url": "/holypulse2-test/static/js/main.29e18534.chunk.js"
+  },
+  {
+    "revision": "e9911e9fb15cfcba638d",
+    "url": "/holypulse2-test/static/js/runtime-main.b1621726.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/holypulse2-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
